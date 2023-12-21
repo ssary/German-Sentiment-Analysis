de348f8119f28184cf81e321db03cd58698d5903
sary nasser <53094371+ssary@users.noreply.github.com>
2023-12-21 23:10:25 +0100

    Abstract database operations with ORM

--- Inference_app.py
+++ Inference_app.py
@@ -2,6 +2,134 @@ import psycopg2
 from datetime import datetime
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData
+
+Base = declarative_base()
+
+class Review(Base):
+    __tablename__ = 'review'
+    review_id = Column(Integer, primary_key=True)
+    review = Column(String)
+    timestamp = Column(DateTime)
+
+class PredictedReviewSentiment(Base):
+    __tablename__ = 'predicted_review_sentiment'
+    predicted_sentiment_id = Column(Integer, primary_key=True)
+    review_id = Column(Integer)
+    sentiment = Column(String)
+
+class ActualReviewSentiment(Base):
+    __tablename__ = 'actual_review_sentiment'
+    actual_sentiment_id = Column(Integer, primary_key=True)
+    review_id = Column(Integer)
+    sentiment = Column(String)
+
+class DatabaseManager:
+    def __init__(self, db_url):
+        """
+        Initialize the database connection using the provided URL.
+
+        Args:
+        - db_url (str): A string containing the database URL.
+        """
+        self.engine = create_engine(db_url)
+        self.meta = MetaData()
+        self.meta.reflect(bind=self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+
+    def add_review(self, review_text, timestamp):
+        """
+        Add a review to the database.
+
+        Args:
+        - review_text (str): The text content of the review.
+        - timestamp (datetime.datetime): The timestamp when the review was created or submitted.
+
+        Returns:
+        - int: The primary key ID of the newly added review record.
+        """
+        session = self.Session()
+        new_review = Review(review=review_text, timestamp=timestamp)
+        session.add(new_review)
+        session.commit()
+        review_id = new_review.review_id
+        session.close()
+        return review_id
+
+    def add_predicted_sentiment(self, review_id, sentiment):
+        """
+        Add a predicted sentiment to the database.
+
+        Args:
+        - review_id (int): The ID of the review for which the sentiment is predicted.
+        - sentiment (str): The predicted sentiment (e.g., 'positive', 'negative', 'neutral').
+
+        Returns:
+        None
+        """
+        session = self.Session()
+        prediction = PredictedReviewSentiment(review_id=review_id, sentiment=sentiment)
+        session.add(prediction)
+        session.commit()
+        session.close()
+
+    def add_actual_sentiment(self, review_id, sentiment):
+        """
+        Add an actual sentiment to the database.
+
+        Args:
+        - review_id (int): The ID of the review for which the actual sentiment is provided.
+        - sentiment (str): The actual sentiment (e.g., 'positive', 'negative', 'neutral').
+
+        Returns:
+        None
+        """
+        session = self.Session()
+        actual_sentiment = ActualReviewSentiment(review_id=review_id, sentiment=sentiment)
+        session.add(actual_sentiment)
+        session.commit()
+        session.close()
+
+    def get_last_entries(self, table, limit=3):
+        """
+        Get the last 'n' entries from a specific table.
+
+        Args:
+        - table (sqlalchemy.ext.declarative.api.DeclarativeMeta): The SQLAlchemy table class representing the database table.
+        - limit (int, optional): The number of entries to retrieve. Default is 3.
+
+        Returns:
+        - list: A list of ORM instances representing the last 'n' rows of the table.
+        """
+        session = self.Session()
+        # Find the primary key column
+        primary_key_column = table.__table__.primary_key.columns.values()[0]
+        entries = session.query(table).order_by(primary_key_column.desc()).limit(limit).all()
+        session.close()
+        return entries
+    
+    def print_last_n_rows_all_tables(self, limit=3):
+        """
+        Prints the last 'n' rows of all tables in the database.
+
+        Args:
+        - limit (int, optional): The number of entries to retrieve from each table. Default is 3.
+
+        Returns:
+        None
+        """
+        session = self.Session()
+        for table_name, table in self.meta.tables.items():
+            print(f'\n{table_name} table:')
+            primary_key_column = table.primary_key.columns.values()[0]
+            last_rows = session.query(table).order_by(primary_key_column.desc()).limit(limit).all()
+            for row in last_rows:
+                print(row)
+        session.close()
+
 
 class SentimentModel:
     def __init__(self, model_path):
@@ -31,74 +159,11 @@ class SentimentModel:
         sentiment_classes = ['negative', 'neutral', 'positive']
         return sentiment_classes[predictions.argmax()]
 
+model_path = 'E:\\HTW\\papers\\Sentiment analysis\\XLM-RoBERTa model'  # or use a model identifier
+sentiment_model = SentimentModel(model_path)
 
-def database_connection():
-    db_config = {
-    "host": "localhost",
-    "port": 5432,
-    "database": "german_sentiment",
-    "user": "postgres",
-    "password": "startengine1"
-    }
-    try:
-        # Establishing the connection
-        conn = psycopg2.connect(**db_config)
-
-        # Creating a cursor object
-        cursor = conn.cursor()
-        cursor.execute("SELECT version();")
-        record = cursor.fetchone()
-        print("You are connected to - ", record, "\n")
-        return conn, cursor
-    except(Exception, psycopg2.Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-        if conn:
-            conn.close()
-
-def process_input(text, tokenizer, model):
-    '''Tokenize the text, apply inference and return the sentiment of the text'''
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=512)
-    outputs = model(**inputs)
-    predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
-    sentiment_classes = ['negative', 'neutral', 'positive']
-    sentiment = sentiment_classes[predictions.argmax()]
-    return sentiment
-
-def insert_review(conn, cursor, review, timestamp):
-    '''Insert review text+timestamp into review table, returns id of the added review'''
-    insertion_query = 'INSERT INTO review(review, timestamp) VALUES(%s, %s) RETURNING review_id'
-    cursor.execute(insertion_query, (review, timestamp))
-    conn.commit()
-    return cursor.fetchone()[0]
-
-def insert_sentiment_prediction(conn, cursor, review_id, sentiment):
-    '''Insert the predicted sentiment of the text into predicted_review_sentiment'''
-    query = 'INSERT INTO predicted_review_sentiment(review_id, sentiment) VALUES(%s, %s)'
-    cursor.execute(query, (review_id, sentiment))
-    conn.commit()
-
-def insert_actual_sentiment_prediction(conn, cursor, review_id, sentiment):
-    '''Insert the ground truth sentiment into actual_review_sentiment'''
-    query = 'INSERT INTO actual_review_sentiment(review_id, sentiment) VALUES(%s, %s)'
-    cursor.execute(query, (review_id, sentiment))
-    conn.commit()
-
-def view_table(cursor, table_name, id_name):
-    '''Show the last 3 rows in the table'''
-    query = f'SELECT * from {table_name} ORDER BY {id_name} DESC LIMIT 3'
-    cursor.execute(query)
-    row = cursor.fetchone()
-    while row is not None:
-        print(row)
-        row = cursor.fetchone()
-
-
-# def row_insertion(review, predicted_review_sentiment, actual_review_sentiment, aspects, actual_aspects, )
-MODEL_PATH = 'E:\HTW\papers\Sentiment analysis\XLM-RoBERTa model'
-
-#Load the pretrained model and it's tokenizer from file
-xlm_model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
-xlm_tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+db_url = "postgresql://postgres:startengine1@localhost:5432/german_sentiment"
+db_manager = DatabaseManager(db_url)
 
 '''
 Doing it from the cloud instead of file
@@ -108,9 +173,6 @@ model = AutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 '''
-
-# connecting to the database
-conn, cursor = database_connection()
 
 print("Enter the reviews, one review on each line, enter empty line to get the sentiments and exit")
 while True:
@@ -126,7 +188,7 @@ while True:
 
     # Iterate over the reviews one by one
     for review in reviews:
-        predicted_sentiment = process_input(user_text, xlm_tokenizer, xlm_model)
+        predicted_sentiment = sentiment_model.predict_sentiment(user_text)
         print(f'sentiment for "{user_text}" is {predicted_sentiment}')
 
         actual_sentiment = input("Please specify the sentiment(positive, negative, neutral) of the sentence:").lower()
@@ -138,23 +200,17 @@ while True:
         aspect_sentiments = aspect_sentiments.split(',')
 
         # Insert review to the review table and return the id
-        review_id = insert_review(conn, cursor, review, timenow)
+        review_id = db_manager.add_review(review, datetime.now())
 
         # Insert the prediction alongwith the review_id
-        insert_sentiment_prediction(conn, cursor, review_id, predicted_sentiment)
-        
+        db_manager.add_predicted_sentiment(review_id, predicted_sentiment)
+
         # Insert actual sentiment if the user entered it correctly
         if actual_sentiment in ['positive', 'negative', 'neutral']:
-            insert_actual_sentiment_prediction(conn, cursor, review_id, actual_sentiment)
-        
-        print('\nreviews table')
-        view_table(cursor, 'review', 'review_id')
-        print('\npredicted reviews sentiment table')
-        view_table(cursor, 'predicted_review_sentiment', 'predicted_sentiment_id')
-        print('\nactual_review_sentiment table')
-        view_table(cursor, 'actual_review_sentiment', 'actual_sentiment_id')
+            db_manager.add_actual_sentiment(review_id, actual_sentiment)
+
+        #Fetching last three rows of each table in the database
+        db_manager.print_last_n_rows_all_tables()
 
 # Closing the cursor and connection
 print('Ciao !')
-cursor.close()
-conn.close()
